efa2b52f57c0dce24b6c98882ca87cdef411431e
Jérôme Benoit <jerome.benoit@sap.com>
2026-02-18 21:52:45 +0100

    fix(build): polyfill require() for ESM bundles (#47)

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
+    banner: ({ format }) => {
+      // Polyfill require() for bundled CJS dependencies in ESM output
+      if (format === "esm") {
+        return {
+          js: `import {createRequire as __createRequire} from 'module';var require=__createRequire(import.meta.url);`,
+        };
+      }
+    },
     clean: true,
     define: {
       __PACKAGE_VERSION__: JSON.stringify(
