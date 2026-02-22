2b6ad261bea8dfb88f9decff695bdec3ac8908c6
Jérôme Benoit <jerome.benoit@piment-noir.org>
2026-02-22 22:09:23 +0100

    chore: refine code coverage thresholds
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- vitest.node.config.ts
+++ vitest.node.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "html", "lcov"],
       thresholds: {
-        branches: 85,
+        branches: 90,
         functions: 90,
         lines: 90,
         statements: 90,
